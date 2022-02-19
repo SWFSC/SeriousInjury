c39186628dbd8109d1e3cc83faf1edb4be3bc89c
JimCarretta <Jim.Carretta@noaa.gov>
2022-02-18 20:37:36 -0800

    tutorial location

---
+++ R/RFSeriousInjury.R
@@ -0,0 +1,25 @@
+#' @title RFSeriousInjuryTutorial()
+#'
+#' @description
+#'
+#' Random Forest (RF) Method For Assessment of Whale Injuries
+#'
+#' Package includes example data.frame 'whales'
+#' (n=20 records) for use with WhaleInjuryCovariates
+#'
+#' @usage
+#' RFSeriousInjuryTutorial()
+#'
+#' @param x an object of class 'data.frame'
+#'
+#' @author Jim Carretta <jim.carretta@noaa.gov>
+#'
+#' @examples WhaleInjuryCovariates(whales)
+#'
+#' @export
+
+ RFSeriousInjuryTutorial = function(x) {
+
+ browseURL("https://github.com/JimCarretta/SeriousInjury/blob/main/_site/index.html")
+
+     }
