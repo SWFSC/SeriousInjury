e7ea7a07fd6383ba9bba6d673a0934e68baf6fc3
JimCarretta <Jim.Carretta@noaa.gov>
2022-02-19 10:58:12 -0800

    edits

--- R/RFSeriousInjury.R
+++ R/RFSeriousInjury.R
@@ -1,32 +1,28 @@
-#' @title SeriousInjuryTutorial()
+#' @title SeriousInjury()
 #'
 #' @description
 #'
 #' Random Forest (RF) Method For Assessment of Whale Injuries
 #'
-#' Package includes example data.frame 'whales'
-#' (n=20 records) for use with WhaleInjuryCovariates()
 #'
 #' @usage
-#' SeriousInjuryTutorial()
-#'
-#' @param x an object of class 'data.frame'
+#' SeriousInjury()
 #'
 #' @author Jim Carretta <jim.carretta@noaa.gov>
 #'
-#' @examples SeriousInjuryTutorial()
+#' @examples
 #'
-#' @export SeriousInjury
+#' @export
 #'
 #'
 
-#SeriousInjuryTutorial <- function() {
-#  utils::browseURL(system.file("index.html", package = "WhaleInjuryCovariates"))
-#}
+SeriousInjuryTutorial <- function() {
+  utils::browseURL(system.file("index.html", package = "SeriousInjury"))
+}
 
-#.onAttach <- function(libname, pkgname) {
-#  packageStartupMessage(
-#    "Welcome to SeriousInjury v", utils::packageVersion("SeriousInjury"), "\n",
-#    "See SeriousInjuryTutorial() for a guide to the package."
-#  )
-#}
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Welcome to SeriousInjury v", utils::packageVersion("SeriousInjury"), "\n",
+    "See SeriousInjuryTutorial() for a guide to the package."
+  )
+}
