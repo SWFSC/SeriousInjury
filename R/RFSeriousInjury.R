1735b5d34b13f7ecb791aa7fad102db4bea4a563
JimCarretta <Jim.Carretta@noaa.gov>
2022-02-18 21:20:02 -0800

    updates

--- R/RFSeriousInjury.R
+++ R/RFSeriousInjury.R
@@ -1,11 +1,11 @@
-#' @title RFSeriousInjuryTutorial()
+#' @title SeriousInjury()
 #'
 #' @description
 #'
 #' Random Forest (RF) Method For Assessment of Whale Injuries
 #'
 #' Package includes example data.frame 'whales'
-#' (n=20 records) for use with WhaleInjuryCovariates
+#' (n=20 records) for use with WhaleInjuryCovariates()
 #'
 #' @usage
 #' RFSeriousInjuryTutorial()
@@ -17,9 +17,16 @@
 #' @examples WhaleInjuryCovariates(whales)
 #'
 #' @export
+#'
+#'
 
- RFSeriousInjuryTutorial = function(x) {
+SeriousInjuryTutorial <- function() {
+  utils::browseURL(system.file("index.html", package = "SeriousInjury"))
+}
 
- browseURL("https://github.com/JimCarretta/SeriousInjury/blob/main/_site/index.html")
-
-     }
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Welcome to SeriousInjury v", utils::packageVersion("SeriousInjury"), "\n",
+    "See SeriousInjuryTutorial() for a guide to the package."
+  )
+}
