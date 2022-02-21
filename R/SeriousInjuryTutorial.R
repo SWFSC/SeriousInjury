74d8f0aeea698d9986d01f683d0a73667df4af5c
JimCarretta <Jim.Carretta@noaa.gov>
2022-02-21 13:27:33 -0800

    Tutorial updated functions

--- R/SeriousInjuryTutorial.R
+++ R/SeriousInjuryTutorial.R
@@ -4,14 +4,14 @@
 #'
 #' @export
 
-SeriousInjuryTutorial <- function() {
+ SeriousInjuryTutorial <- function() {
 
-  utils::browseURL("https://jimcarretta.github.io/index.html")
-}
+  SeriousInjuryTutorial() <- browseURL("https://jimcarretta.github.io/index.html")
+ }
 
-onAttach <- function(libname, pkgname) {
+ onAttach <- function(libname, pkgname) {
   packageStartupMessage(
     "Welcome to Serious Injury v", utils::packageVersion("SeriousInjury"), "\n",
     "See SeriousInjuryTutorial() for a package description."
   )
-}
+ }
