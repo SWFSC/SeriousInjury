f356ec3be78ef8d85b1fcf231480f20c2a66dab4
JimCarretta <Jim.Carretta@noaa.gov>
2022-02-23 15:05:25 -0800

    Deleted NOAA logo from HTML pages

---
+++ _site/R/SeriousInjury_Package.R
@@ -0,0 +1,22 @@
+#' @title SeriousInjuryTutorial
+#'
+#' @description Package to assess large whale injuries with Random Forests
+#'
+#' @usage SeriousInjuryTutorial()
+#'
+#' @author Jim Carretta <jim.carretta@noaa.gov>
+#'
+#' @examples
+#'
+#' @export SeriousInjuryTutorial
+#'
+SeriousInjuryTutorial <- function() {
+  browseURL("https://JimCarretta.github.io/index.html")
+}
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    "Welcome to SeriousInjury v", utils::packageVersion("SeriousInjury"), "\n",
+    "See SeriousInjuryTutorial() for a guide to the package."
+  )
+}
