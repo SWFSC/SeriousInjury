ce62565c49d24d0a54ceab509b1c7cc4f84f1145
Jim Carretta <jim.carretta@noaa.gov>
2024-02-19 13:56:38 -0800

    shiny ui updates

--- inst/shinyapp/SeriousInjury/ui.R
+++ inst/shinyapp/SeriousInjury/ui.R
@@ -1,14 +1,29 @@
 .libPaths(c("/usr/lib64/R/shiny_library",.libPaths()))
 
 ui <- fluidPage(
-    titlePanel("R-Package *SeriousInjury*: Assess severity of whale injuries from a narrative."),
-    mainPanel(
+    tags$head(
+        tags$style(HTML("
+      /* Increase font size for all text elements */
+      body {
+        font-size: 24px; /* Adjust the font size as needed */
+      }
+
+      /* Increase font size for specific element by class */
+      .title {
+        font-size: 18px; /* Adjust the font size as needed */
+      }
+    "))
+    ),
+
+
+    titlePanel("R-Package SeriousInjury. Assess severity of whale injuries from a narrative."),
+    mainPanel(actionButton("Add", "Update Narrative"),
         textAreaInput(inputId = "Narrative",
                       label = "Narrative",
-                      value = "Type/paste text and press ʻEnterʻ button for injury assessment. Use toggle to switch between entanglements or vessel strikes. Whale with multiple wraps of line around peduncle and heavy cyamid growth.", width='400px', height='400px')),
-    selectInput("Injury.Type", "EN (entanglement) or VS (vessel strike)",
+                      value = "Type/paste text. Press ʻUpdate Narrativeʻ button for injury assessment. Toggle to switch between entanglements or vessel strikes. Whale with multiple wraps of line around peduncle and heavy cyamid growth.", width='400px', height='400px')),
+
+               selectInput("Injury.Type", "EN (entanglement) or VS (vessel strike)",
                 list(`Injury.Type` = list("EN", "VS"))),
-    actionButton("Add", "Enter"),
 
     mainPanel(tabsetPanel(
         tabPanel("Predicted Probability of Death|Health Decline or Recovery", tableOutput(outputId = "table")))))
