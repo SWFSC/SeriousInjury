2c3b120472e31cf6b0d4788350040f4a7148b822
Jim Carretta <Jim.Carretta@noaa.gov>
2024-11-20 20:03:16 -0800

    Add files via upload

--- inst/shinyapp/SeriousInjury/ui.R
+++ inst/shinyapp/SeriousInjury/ui.R
@@ -22,9 +22,9 @@ ui <- fluidPage(
 
     titlePanel(em("R-Package SeriousInjury: Choose Entanglement *or* Vessel Strike to run appropriate model.")),
     fluidRow(
-        column(3, checkboxGroupInput("Injury.Type", "", choices = c("Entanglement", "Vessel Strike", "Calf or Juvenile", "Mobility.Limited (anchored, reduced mobility)", "Constricting Gear", "Non-Constricting or Loose Gear likely to be shed (lack of constricting gear must be confirmed)"))),
+        column(3, checkboxGroupInput("Injury.Type", "", choices = c("Entanglement", "Vessel Strike", "Calf or Juvenile", "Mobility.Limited (anchored, reduced mobility)", "Constricting Gear", "Confirmed loose or non-constricting gear likely to be shed, e.g. monofilament hook and line)"))),
         column(3, checkboxGroupInput("checkGroup2", "", choices = c("Injury involves head, mouth, or blowhole?", "Fluke or Peduncle involvement", "Free-swimming and/or Diving (opposite of Mobility.Limited)","Health Decline (cyamids, emaciation, skin discoloration)", "Gear-free", "Pectoral Flipper involvement", "Trailing Gear"))),
-        column(3, checkboxGroupInput("checkGroup3", "", choices = c("Wraps Present (one or multiple wraps of gear)", "Wraps Absent (must be confirmed)", "Evidence of Healing", "Laceration Deep", "Laceration Shallow", "Monofilament Hook & Line (exludes gillnets)", "Severe (extensive injuries)"))),
+        column(3, checkboxGroupInput("checkGroup3", "", choices = c("Wraps Present (one or multiple wraps of gear)", "Wraps Absent (must be confirmed)", "Evidence of Healing", "Laceration Deep", "Laceration Shallow or Absent", "Severe (extensive injuries)"))),
         column(3, checkboxGroupInput("checkGroup4", "", choices = c("Vessel Fast >10kt", "Vessel Slow <=10kt", "Vessel Speed Unknown", "Vessel Large >=65ft", "Vessel Small <65ft", "Vessel Size Unknown")),
 
                actionButton("submit", "Submit"))),
